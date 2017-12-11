2b0a0b60eb58decfca2d0af38b126ac0469fed93
Charles Seymour <charlesseymour@yahoo.com>
2017-12-11 10:40:09 -0600

    Added post, update, and delete methods

--- resources/todos.py
+++ resources/todos.py
@@ -28,6 +28,35 @@ class TodoList(Resource):
         todos = [marshal(todo, todo_fields)
                  for todo in models.Todo.select()]
         return {'todos': todos}
+    
+    @marshal_with(todo_fields)
+    def post(self):
+        args = self.reqparse.parse_args()
+        todo = models.Todo.create(**args)
+        return todo, 201
+
+class Todo(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'name',
+            required=True,
+            help='No name provided',
+            location=['form', 'json']    
+        )
+        super().__init__()
+
+    @marshal_with(todo_fields)    
+    def put(self, id):
+        args = self.reqparse.parse_args()
+        query = models.Todo.update(**args).where(models.Todo.id==id)
+        query.execute()
+        return models.Todo.get(models.Todo.id==id), 200
+
+    def delete(self, id):
+        query = models.Todo.delete().where(models.Todo.id==id)
+        query.execute()
+        return '', 204
 
 todos_api = Blueprint('resources.todos', __name__)
 api = Api(todos_api)
@@ -36,4 +65,8 @@ api.add_resource(
     '/api/v1/todos',
     endpoint='todos'
 )
-        
+api.add_resource(
+    Todo,
+    '/api/v1/todos/<int:id>',
+    endpoint='todo'
+)        
