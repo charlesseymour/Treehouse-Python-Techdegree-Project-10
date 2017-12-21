93144675a69ed80a36451a0ca4ee4abd7ee74b1c
Charles Seymour <charlesseymour@yahoo.com>
2017-12-21 09:14:48 -0600

    Changed get resource to properly display todos on front page

--- resources/todos.py
+++ resources/todos.py
@@ -28,7 +28,7 @@ class TodoList(Resource):
     def get(self):
         todos = [marshal(todo, todo_fields)
                  for todo in models.Todo.select()]
-        return {'todos': todos}
+        return todos
 
     @marshal_with(todo_fields)
     @auth.login_required
